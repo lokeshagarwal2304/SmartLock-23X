8ba00965e52b70093f36fc0732a3c229e14c93ac
Lokesh Agarwal <145140240+lokeshagarwal2304@users.noreply.github.com>
2025-08-14 16:46:23 +0530

    Create buzzer_alert.h

---
+++ include/buzzer_alert.h
@@ -0,0 +1,16 @@
+#ifndef BUZZER_ALERT_H
+#define BUZZER_ALERT_H
+
+// Function to initialize buzzer
+void buzzer_init();
+
+// Function to turn buzzer ON
+void buzzer_on();
+
+// Function to turn buzzer OFF
+void buzzer_off();
+
+// Function to beep buzzer for given ms
+void buzzer_beep(int durationMs);
+
+#endif // BUZZER_ALERT_H
